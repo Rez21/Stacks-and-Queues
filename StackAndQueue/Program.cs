5cea028d9b4bf8505e8c5afc62676bb859fe5a27
Prasad <banprasad21@gmail.com>
2022-09-09 01:37:04 +0530

    [Raghav] Add: create queue and add elements

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -4,17 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Stack stack = new Stack();
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
-            stack.Display();
-            stack.Peak();
-            stack.Pop();
-            stack.Peak();
-            stack.Pop();
-            stack.Peak();
-            stack.Pop();
+            //Stack stack = new Stack();
+            //stack.Push(70);
+            //stack.Push(30);
+            //stack.Push(56);
+            //stack.Display();
+            //stack.Peak();
+            //stack.Pop();
+            //stack.Peak();
+            //stack.Pop();
+            //stack.Peak();
+            //stack.Pop();
+            Queue queue = new Queue();
+            queue.Append(56);
+            queue.Append(30);
+            queue.Append(70);
+            queue.Display();
         }   
     }    
 }
